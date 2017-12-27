47bedcf2f2e20568de1a6b38868b6c5a6c5e4415
Cristiano Maia <criscmaia@gmail.com>
2017-12-27 20:28:19 +0000

    get papers and check if author name is on the DB

--- collectMdxPapers.php
+++ collectMdxPapers.php
@@ -3,19 +3,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'dbconnect.php';
-
 function check_mdxAuthorExistence($fname, $lname){
     include 'dbconnect.php';
-    $chk = $conn->query("SELECT * FROM mdxAuthor WHERE firstName = '$fname' AND lastName = '$lname';") ;
-    if($chk->num_rows==0){
-        echo $fname . " " . $lname . " not in the DB.";
-        //$conn->query("INSERT INTO `creators` (  `creator_family_name`, `creator_given_name`, `project_id`) VALUES ('$fname', '$gname', '$project_id')");
-        //return $conn->getLastId();
-    } else{
-        echo $fname . " " . $lname . " already in the DB. (id: " . $chk->row['id'] . ")";
-        //$a = $chk->row;
-        //return $a['creator_id'];
+
+    $fullName = $fname . ' ' . $lname;
+    if ($checkMdxAuthorExistence = $conn->query("SELECT * FROM mdxAuthor WHERE CONCAT(firstName, ' ', lastName) like '$fullName';")) {
+        $row_cnt = $checkMdxAuthorExistence->num_rows;
+        if($row_cnt>0) {
+            $resultsArray = $checkMdxAuthorExistence->fetch_assoc();
+            echo $fname . " " . $lname. " is IN THE DB. ID: " . $resultsArray['mdxAuthorID'] . " <br>";
+        }
+        $checkMdxAuthorExistence->close();
     }
 }
 
@@ -34,103 +32,60 @@ echo "<pre>" . $jsonData . "</pre>";
 echo "<hr>";
 
 foreach($json as $indjson){
-    $te = $indjson;
-    $type = $te->type;
+    $paper = $indjson;
+    $type = $paper->type;
     $abstract = "";
-    if($te->abstract){
-        $abstract = $te->abstract;
-        echo $abstract . "<br>";
+    if(isset($paper->abstract)){
+        $abstract = $paper->abstract;
     }
-
-    /*
-    conference_item
-    article
-    book_section
-    other
-    book
-    monograph
-    thesis
-    video
-    */
 
     $extra_info = "";
 
     if($type=='conference_item'){
-//        $extra_info = mysql_real_escape_string($te->event_title);
-         $extra_info = $te->event_title;
+         $extra_info = $paper->event_title;
     }
 
     if($type=='article'){
-//        $extra_info = mysql_real_escape_string($te->publication);
-        $extra_info = $te->publication;
+        $extra_info = $paper->publication;
     }
-
 
     if($type=='book_section'){
-//        $extra_info = mysql_real_escape_string($te->book_title);
-        $extra_info = $te->book_title;
+        $extra_info = $paper->book_title;
     }
 
-    echo $extra_info;
-
     // Where the paper was published. So if it was an article, it will be the name of the journal. If it was a conference item then the name of the conference, and it was  a book section, the name of the book.
-
-//    $eprintid = $te->eprintid;
-//    $succeeds = mysql_real_escape_string($te->succeeds);
-//    $allcreators = $te->creators ;
-//    $title =  mysql_real_escape_string($te->title);
-//    $date =  mysql_real_escape_string($te->date);
-//    $ispublished =  mysql_real_escape_string($te->ispublished);
-//    $pres_type = mysql_real_escape_string($te->pres_type);
-//    $keywords = mysql_real_escape_string($te->keywords);
-//    $publication = mysql_real_escape_string($te->publication);
-//    $volume = mysql_real_escape_string($te->volume);
-//    $number =  mysql_real_escape_string($te->number);
-//    $publisher =  mysql_real_escape_string($te->publisher);
-//    $uri =  mysql_real_escape_string($te->uri);
-//    $doi =  mysql_real_escape_string($te->official_url);
-//    $event_title = mysql_real_escape_string($te->event_title);
-//    $event_type =  mysql_real_escape_string($te->event_type);
-//    $isbn =  mysql_real_escape_string($te->isbn);
-//    $issn =  mysql_real_escape_string($te->issn);
-
-    $eprintid = $te->eprintid;
-    $succeeds = $te->succeeds;
-    $allcreators = $te->creators ;
-    $title =  $te->title;
-    $date =  $te->date;
-    $ispublished = $te->ispublished;
-    $pres_type = $te->pres_type;
-    $keywords = $te->keywords;
-    $publication = $te->publication;
-    $volume = $te->volume;
-    $number =  $te->number;
-    $publisher = $te->publisher;
-    $uri =  $te->uri;
-    $doi =  $te->official_url;
-    $event_title = $te->event_title;
-    $event_type =  $te->event_type;
-    $isbn =  $te->isbn;
-    $issn =  $te->issn;
+    $eprintid = $paper->eprintid;
+    if (isset($paper->succeeds))     { $succeeds = $paper->succeeds; }
+    if (isset($paper->creators))     { $allcreators = $paper->creators; }
+    if (isset($paper->title))        { $title =  $paper->title; }
+    if (isset($paper->date))         { $date =  $paper->date; }
+    if (isset($paper->ispublished))  { $ispublished = $paper->ispublished; }
+    if (isset($paper->pres_type))    { $pres_type = $paper->pres_type; }
+    if (isset($paper->keywords))     { $keywords = $paper->keywords; }
+    if (isset($paper->publication))  { $publication = $paper->publication; }
+    if (isset($paper->volume))       { $volume = $paper->volume; }
+    if (isset($paper->number))       { $number =  $paper->number; }
+    if (isset($paper->publisher))    { $publisher = $paper->publisher; }
+    if (isset($paper->uri))          { $uri =  $paper->uri; }
+    if (isset($paper->official_url)) { $doi =  $paper->official_url; }
+    if (isset($paper->event_title))  { $event_title = $paper->event_title; }
+    if (isset($paper->event_type))   { $event_type =  $paper->event_type; }
+    if (isset($paper->isbn))         { $isbn =  $paper->isbn; }
+    if (isset($paper->issn))         { $issn = $paper->issn; }
 
     if(sizeof($allcreators)>0){
-        echo "allcreators > 0";
         foreach($allcreators as $eachcreator){
-//            $g = mysql_real_escape_string($eachcreator->name->given);
-//            $f = mysql_real_escape_string($eachcreator->name->family);
-
-            $g = $eachcreator->name->given;
-            $f = $eachcreator->name->family;
-            $creator_ids .= '|'. check_mdxAuthorExistence($f, $g);
+            $fName = $eachcreator->name->given;
+            $lName = $eachcreator->name->family;
+            check_mdxAuthorExistence($fName, $lName);
         }
-        $creator_ids .='|0';
     }
 
 
 
     $existing_eprints_array = array();
-//    $book_title =  mysql_real_escape_string($te->book_title);
-//    $book_title =  $te->book_title;
+//    $book_title =  mysql_real_escape_string($paper->book_title);
+//    $book_title =  $paper->book_title;
     $split_date = explode('-',$date);
     $year = $split_date[0];
     if($title!="" && $year>=2014){
@@ -139,13 +94,7 @@ foreach($json as $indjson){
             // $conn->query("INSERT INTO `collections` (`project_id`, `type`, `creators`, `succeeds`, `title`, `ispublished`, `pres_type`, `keywords`, `publication`, `volume`, `number`, `publisher`, `event_title`, `event_type`, `isbn`, `issn`, `book_title`, `ePrintID`, `doi`, `uri`, `date`,`additional_info`,`abstract`) VALUES ('$project_id', '$type', '$creator_ids', '$succeeds', '$title', '$ispublished', '$pres_type', '$keywords', '$publication', '$volume', '$number', '$publisher', '$event_title', '$event_type', '$isbn', '$issn', '$book_title','$eprintid','$doi','$uri','$date','$extra_info','$abstract')");
             $existing_eprints_array[$eprintid] = $title;
 //            $collection_id = $conn->getLastId();
-            $creator_ids_arr = explode('|', $creator_ids);
             $results_found = 0;
-
-            for($i=1;$i<sizeof($creator_ids_arr);$i++){
-                //"INSERT INTO `creators_collection` ( `creator_id`, `collection_id`) VALUES ( '$creator_ids[$i]', '$collection_id')<br>";
-                //mysql_query("INSERT INTO `creators_collection` ( `creator_id`, `collection_id`) VALUES ( '$creator_ids_arr[$i]', '$collection_id')");
-            }
         }
         $results_found++;
     }
