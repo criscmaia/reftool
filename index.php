f6d90f90eba0bc3bd65ca7679d5cd2fd6bb3def2
Cristiano Maia <criscmaia@gmail.com>
2017-12-27 15:18:02 +0000

    index + mdxauthor and some cleaning code

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags. -->
+    <title>refTool</title>
+</head>
+
+<body>
+    <h1>RefTool</h1>
+    <?php
+        include 'mdxAuthor.php';
+    ?>
+</body>
+
+</html>
--- mdxAuthor.php
+++ mdxAuthor.php
@@ -1,48 +1,35 @@
-<!DOCTYPE html>
-<html>
+<h2>Search MDX author</h2>
+<form method="post" action="<?=$_SERVER['PHP_SELF']?>#results" method="post">
+    <p><label>First Name:     <input type="text" required maxlength=100 name="firstName"></label></p>
+    <p><label>Last Name:      <input type="text" required maxlength=200 name="lastName"></label></p>
+    <p><button type="submit" name="searchMdxAuthor">SEARCH FOR MDX AUTHOR</button></p>
+</form>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags. -->
-    <title>refTool</title>
-</head>
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-<body>
-    <h1>RefTool</h1>
-    <form method="post" action="<?=$_SERVER['PHP_SELF']?>#results" method="post">
-        <p><label>First Name:     <input type="text" required maxlength=100 name="firstName"></label></p>
-        <p><label>Last Name:      <input type="text" required maxlength=200 name="lastName"></label></p>
-        <p><button type="submit" name="searchMdxAuthor">SEARCH FOR MDX AUTHOR</button></p>
-    </form>
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        include 'dbconnect.php';
 
-    <?php
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+        // CHECK IF MDX AUTHOR ALREADY EXISTS
+        $firstNameForm = mysqli_real_escape_string($conn,  $_POST['firstName']);
+        $lastNameForm = mysqli_real_escape_string($conn,  $_POST['lastName']);
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            include 'dbconnect.php';
+        if ($checkMdxAuthorExistence = $conn->query("SELECT * FROM mdxAuthor WHERE firstName = '$firstNameForm' AND lastName = '$lastNameForm';")) {
+            $row_cnt = $checkMdxAuthorExistence->num_rows;
+            echo "<p>Result set has $row_cnt row(s).</p>";
 
-            // CHECK IF MDX AUTHOR ALREADY EXISTS
-            if ($checkMdxAuthorExistence = $conn->query("SELECT * FROM mdxAuthor WHERE firstName = '".mysqli_real_escape_string($conn,  $_POST['firstName'])."' AND lastName = '".mysqli_real_escape_string($conn,  $_POST['lastName'])."';")) {
-                $row_cnt = $checkMdxAuthorExistence->num_rows;
-                echo "<p>Result set has $row_cnt row(s).</p>";
-
-                // IF CAN'T FIND ANY AUTHOR
-                if($row_cnt==0) {
-                    echo 'MDX author not found';
-                } else {
-                    echo 'MDX author found';
-                }
-
-                /* close connection */
-                $conn->close();
+            // IF CAN'T FIND ANY AUTHOR
+            if($row_cnt==0) {
+                echo 'MDX author not found';
+            } else {
+                echo 'MDX author found';
             }
+
+            /* close connection */
+            $conn->close();
         }
-    ?>
-
-</body>
-
-</html>
+    }
+?>
