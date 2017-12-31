8fe5828b677855f388f14859329abd416abd32c7
Cristiano Maia <criscmaia@gmail.com>
2017-12-31 15:57:58 +0000

    cleaning the code/columns

--- collectMdxPapers.php
+++ collectMdxPapers.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// check if author is on the DB
 function check_mdxAuthorExistence($fname, $lname){
     include 'dbconnect.php';
 
@@ -18,7 +19,7 @@ function check_mdxAuthorExistence($fname, $lname){
 }
 
 
-// check paper grade
+// check paper rank
 function checkEra2010rank($issn) {
     include 'dbconnect.php';
 
@@ -27,6 +28,7 @@ function checkEra2010rank($issn) {
         if($row_cnt>0) {
             $resultsArray = $checkEraRank->fetch_assoc();
             echo $issn . " issn RANK is: " . $resultsArray['rank'] . " <br>";
+            return $resultsArray['rank'];
         }
         $checkEraRank->close();
     }
@@ -44,51 +46,37 @@ $json_str = $result;
 $json = json_decode($json_str);
 
 $jsonData = json_encode($json, JSON_PRETTY_PRINT);
-echo "<pre>" . $jsonData . "</pre>";
-echo "<hr>";
+//echo "<pre>" . $jsonData . "</pre><hr>";
 
 foreach($json as $indjson){
     $paper = $indjson;
-    $type = $paper->type;
-    $abstract = "";
-    if(isset($paper->abstract)){
-        $abstract = $paper->abstract;
-    }
-
-    $extra_info = "";
-
-    if($type=='conference_item'){
-         $extra_info = $paper->event_title;
-    }
-
-    if($type=='article'){
-        $extra_info = $paper->publication;
-    }
-
-    if($type=='book_section'){
-        $extra_info = $paper->book_title;
-    }
-
-    // Where the paper was published. So if it was an article, it will be the name of the journal. If it was a conference item then the name of the conference, and it was  a book section, the name of the book.
-    $eprintid = $paper->eprintid;
+    if (isset($paper->type;))        { $type = $paper->type; }
+//  AUTHOR
+//  if (isset($paper->creators))     { $allcreators = $paper->creators; }
     if (isset($paper->succeeds))     { $succeeds = $paper->succeeds; }
-    if (isset($paper->creators))     { $allcreators = $paper->creators; }
     if (isset($paper->title))        { $title =  $paper->title; }
-    if (isset($paper->date))         { $date =  $paper->date; }
     if (isset($paper->ispublished))  { $ispublished = $paper->ispublished; }
-    if (isset($paper->pres_type))    { $pres_type = $paper->pres_type; }
+    if (isset($paper->pres_type))    { $presType = $paper->pres_type; }
     if (isset($paper->keywords))     { $keywords = $paper->keywords; }
     if (isset($paper->publication))  { $publication = $paper->publication; }
     if (isset($paper->volume))       { $volume = $paper->volume; }
     if (isset($paper->number))       { $number =  $paper->number; }
     if (isset($paper->publisher))    { $publisher = $paper->publisher; }
-    if (isset($paper->uri))          { $uri =  $paper->uri; }
-    if (isset($paper->official_url)) { $doi =  $paper->official_url; }
-    if (isset($paper->event_title))  { $event_title = $paper->event_title; }
-    if (isset($paper->event_type))   { $event_type =  $paper->event_type; }
+    if (isset($paper->event_title))  { $eventTitle = $paper->event_title; }
+    if (isset($paper->event_type))   { $eventType =  $paper->event_type; }
     if (isset($paper->isbn))         { $isbn =  $paper->isbn; }
     if (isset($paper->issn))         { $issn = $paper->issn; }
+    $eraRating = checkEra2010rank($issn);       // check ERA2010 rank based on ISSN
+    if (isset($paper->book_title))   { $bookTitle =  $paper->book_title; }
+    if (isset($paper->eprintid;))    { $eprintid = $paper->eprintid; }
+    if (isset($paper->uri))          { $uri =  $paper->uri; }
+//  $additionalInfo                                                             // not used
+    if(isset($paper->abstract))      { $abstract = $paper->abstract; }
+    if (isset($paper->date))         { $date =  $paper->date; }
+//    if (isset($paper->official_url)) { $doi =  $paper->official_url; }        // not used
 
+
+    // if it has the authors, check if in the DB
     if(sizeof($allcreators)>0){
         foreach($allcreators as $eachcreator){
             $fName = $eachcreator->name->given;
@@ -97,33 +85,17 @@ foreach($json as $indjson){
         }
     }
 
-    checkEra2010rank($issn);
 
 
-    $existing_eprints_array = array();
-//    $book_title =  mysql_real_escape_string($paper->book_title);
-//    $book_title =  $paper->book_title;
+
+
     $split_date = explode('-',$date);
     $year = $split_date[0];
     if($title!="" && $year>=2014){
-        // Full texts 	collection_id 	type 	creators 	succeeds 	title 	ispublished 	pres_type 	keywords 	publication 	volume 	number 	publisher 	event_title 	event_type 	isbn 	issn 	book_title 	ePrintID 	doi 	uri 	additional_info 	abstract
-        if(!array_key_exists($eprintid, $existing_eprints_array)){
-            // $conn->query("INSERT INTO `collections` (`project_id`, `type`, `creators`, `succeeds`, `title`, `ispublished`, `pres_type`, `keywords`, `publication`, `volume`, `number`, `publisher`, `event_title`, `event_type`, `isbn`, `issn`, `book_title`, `ePrintID`, `doi`, `uri`, `date`,`additional_info`,`abstract`) VALUES ('$project_id', '$type', '$creator_ids', '$succeeds', '$title', '$ispublished', '$pres_type', '$keywords', '$publication', '$volume', '$number', '$publisher', '$event_title', '$event_type', '$isbn', '$issn', '$book_title','$eprintid','$doi','$uri','$date','$extra_info','$abstract')");
-            $existing_eprints_array[$eprintid] = $title;
-//            $collection_id = $conn->getLastId();
-            $results_found = 0;
+"INSERT INTO `publication` (`type`,`authors`,`succeeds`,`title`,`isPublished`,`presType`,`keywords`,`publication`,`volume`,`number`,`publisher`,`eventTitle`,`eventType`,`isbn`,`issn`,`bookTitle`,`ePrintID`,`doi`,`uri`,`additionalInfo`,`abstract`,`date`,`eraRating`)
+        VALUES ('$type', 'FK', '$succeeds', '$title', '$ispublished', '$presType', '$keywords', '$publication', '$volume', '$number', '$publisher', '$eventTitle', '$eventType', '$isbn', '$issn', '$bookTitle', '$eprintid', '$uri', '$additionalInfo', '$abstract', '$date', '$eraRating');"
         }
         $results_found++;
     }
 }
-
-$dt = date('Y-m-d H:i:s');
-
-//    $conn->query("update `collection_processes` set `finshed_date_time` = '$dt', results_found = '$results_found' where `process_id` = '$search_id'");
-//    $get_next = $conn->query("select * from `collection_processes` where `process_id` > '$search_id' LIMIT 1");
-//    if($get_next->num_rows>0){
-//        $search_next_id = $get_next->row['process_id'];
-//    } else{
-//        $search_next_id = 0;
-//    }
 ?>
