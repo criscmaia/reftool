b4ad136cd8ef382cc3139ef6a51eaeb5694d8e43
Cristiano Maia <criscmaia@gmail.com>
2018-01-01 12:26:31 +0000

    searche for every person from the imported file :)

--- readExcel.php
+++ readExcel.php
@@ -1,14 +1,13 @@
 <?php
 require_once __DIR__ . '/simplexlsx.class.php';
+session_start();
 
 if ( $xlsx = SimpleXLSX::parse('staff.xlsx')) {
     echo '<h1>$xlsx->rows()</h1>';
     echo '<pre>';
     print_r( $xlsx->rows() );
     echo '</pre>';
-    session_start();
-    $_SESSION['firstName'] = $xlsx->rows()[1][0];
-    $_SESSION['lastName'] = $xlsx->rows()[1][1];
+    $_SESSION['importedNames'] = $xlsx->rows();
 } else {
     echo SimpleXLSX::parse_error();
 }
