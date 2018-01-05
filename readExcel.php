cb9588355c175379db9e344a4d55c4ae8d8ea35a
Cristiano Maia <criscmaia@gmail.com>
2018-01-05 17:11:52 +0000

    1:remove empties from multidimensional array.Dirty

--- readExcel.php
+++ readExcel.php
@@ -2,18 +2,50 @@
 require_once __DIR__ . '/simplexlsx.class.php';
 session_start();
 
-$filePath = $_SESSION['filePath'];
+//$filePath = $_SESSION['filePath'];
+$filePath = "test.xlsx";
 
 if ( $xlsx = SimpleXLSX::parse($filePath)) {
-        $totalNames = count($xlsx->rows());
+    $fullFile = $xlsx->rows();
+    echo '<pre>';
+    print_r( $fullFile );
+    echo '</pre><hr>';
+//        $filtered = array_filter($fullFile);
+//        echo '<pre>';
+
+    var_export(deepFilter($fullFile));
+
+          echo '</pre><hr>';
+    echo '<hr>';
+    $totalNames = count($xlsx->rows());
 //        echo '<h1>$xlsx->rows()</h1>';
-        echo '<pre>';
-        print_r( $xlsx->rows() );
-        echo '</pre>';
-        $_SESSION['importedNames'] = $xlsx->rows();     // save array names to SESSION so 'collectMdxPapers can access it [cm-18.01.01]
-        echo $totalNames . ' names found. <a href="/reftool/collectMdxPapers.php"> Click here to continue ➔</a>';
+    echo '<pre>';
+    print_r( $xlsx->rows() );
+    echo '</pre>';
+    $_SESSION['importedNames'] = $xlsx->rows();     // save array names to SESSION so 'collectMdxPapers can access it [cm-18.01.01]
+    echo $totalNames . ' names found. <a href="/reftool/collectMdxPapers.php"> Click here to continue ➔</a>';
 } else {
     echo SimpleXLSX::parse_error();
 }
+
+function deepFilter(array $array)
+{
+    if (empty($array)) {
+        return [];
+    }
+
+    $filteredArray = [];
+    foreach ($array as $key => $value) {
+        if (is_array($value) && !empty($value)) {
+            $value = deepFilter($value);
+        }
+        if (!empty($value)) {
+            $filteredArray[$key] = $value;
+        }
+    }
+
+    return $filteredArray;
+}
+
 
 ?>
