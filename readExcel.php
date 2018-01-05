eb4007944ea04ccce4affe8f2ceca15a7eb54072
Cristiano Maia <criscmaia@gmail.com>
2018-01-05 17:24:55 +0000

    Filter to remove null values works better now

--- readExcel.php
+++ readExcel.php
@@ -2,50 +2,16 @@
 require_once __DIR__ . '/simplexlsx.class.php';
 session_start();
 
-//$filePath = $_SESSION['filePath'];
-$filePath = "test.xlsx";
+$filePath = $_SESSION['filePath'];
+//$filePath = "test.xlsx";
 
 if ( $xlsx = SimpleXLSX::parse($filePath)) {
-    $fullFile = $xlsx->rows();
     echo '<pre>';
-    print_r( $fullFile );
-    echo '</pre><hr>';
-//        $filtered = array_filter($fullFile);
-//        echo '<pre>';
-
-    var_export(deepFilter($fullFile));
-
-          echo '</pre><hr>';
-    echo '<hr>';
-    $totalNames = count($xlsx->rows());
-//        echo '<h1>$xlsx->rows()</h1>';
-    echo '<pre>';
-    print_r( $xlsx->rows() );
-    echo '</pre>';
-    $_SESSION['importedNames'] = $xlsx->rows();     // save array names to SESSION so 'collectMdxPapers can access it [cm-18.01.01]
+    $filteredFile = array_filter(array_map('array_filter', $xlsx->rows()));     // filter out all keys-values that are empty/null
+    print_r($filteredFile);
+    $totalNames = count($filteredFile);
+    $_SESSION['importedNames'] = $filteredFile;                                 // save array names to SESSION so 'collectMdxPapers can access it
     echo $totalNames . ' names found. <a href="/reftool/collectMdxPapers.php"> Click here to continue ➔</a>';
 } else {
     echo SimpleXLSX::parse_error();
 }
-
-function deepFilter(array $array)
-{
-    if (empty($array)) {
-        return [];
-    }
-
-    $filteredArray = [];
-    foreach ($array as $key => $value) {
-        if (is_array($value) && !empty($value)) {
-            $value = deepFilter($value);
-        }
-        if (!empty($value)) {
-            $filteredArray[$key] = $value;
-        }
-    }
-
-    return $filteredArray;
-}
-
-
-?>
