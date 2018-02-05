5043c088849a2529a9115454d11eb09f9231949d
Cristiano Maia <criscmaia@gmail.com>
2018-02-05 12:08:20 +0000

    minor layout improvements

--- readExcel.php
+++ readExcel.php
@@ -9,10 +9,11 @@ $filePath = $_SESSION['filePath'];
 if ( $xlsx = SimpleXLSX::parse($filePath)) {
     echo '<pre>';
     $filteredFile = array_filter(array_map('array_filter', $xlsx->rows()));     // filter out all keys-values that are empty/null
-    print_r($filteredFile);
     $totalNames = count($filteredFile);
     $_SESSION['importedNames'] = $filteredFile;                                 // save array names to SESSION so 'collectMdxPapers can access it
-    echo $totalNames . ' names found. <a href="/reftool/collectMdxPapers.php"> Click here to continue ➔</a>';
+    echo '<br><strong>' . $totalNames . ' names found</strong>. <br><a href="/reftool/collectMdxPapers.php">Click here to continue ➔</a><br><br><br>';
+    echo '<hr>Full imported content:<br><br>';
+    print_r($filteredFile);
 } else {
     echo SimpleXLSX::parse_error();
 }
