0b44a229e82168afb9ca31c464d6f7b1ba5cff60
Cristiano Maia <criscmaia@gmail.com>
2018-02-23 19:54:58 +0000

    removing head and reading new column

--- readExcel.php
+++ readExcel.php
@@ -8,7 +8,13 @@ $filePath = $_SESSION['filePath'];
 
 if ( $xlsx = SimpleXLSX::parse($filePath)) {
     echo '<pre>';
-    $filteredFile = array_filter(array_map('array_filter', $xlsx->rows()));     // filter out all keys-values that are empty/null
+    // the option below had to be replaced in order to accommodate the new column from the spreasheet
+    // "Current Employee" accepts 1 or 0 and the 0s were being automatically removed
+    //    $filteredFile = array_filter(array_map('array_filter', $xlsx->rows()));     // filter out all keys-values that are empty/null/0s
+
+    $filteredFile = array_filter(array_map('array_filter', $xlsx->rows()), 'strlen');     // remove NULL, FALSE and Empty Strings (""), but leave values of 0
+    $removedTitle = array_shift($filteredFile);                                           // array with removed headings from the spreadsheet. can be ignored.
+
     $totalNames = count($filteredFile);
     $_SESSION['importedNames'] = $filteredFile;                                 // save array names to SESSION so 'collectMdxPapers can access it
     echo '<br><strong>' . $totalNames . ' names found</strong>. <br><a href="/reftool/collectMdxPapers.php">Click here to continue ➔</a><br><br><br>';
