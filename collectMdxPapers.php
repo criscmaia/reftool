8e4c796331a39318bcfa9660b5c3ffaf3668bccb
Cristiano Maia <criscmaia@gmail.com>
2018-04-14 11:58:36 +0100

    so long ago.. have no clue why this was changed

--- collectMdxPapers.php
+++ collectMdxPapers.php
@@ -45,7 +45,7 @@ if(!isset($_SESSION["importedNames"]) && empty($_SESSION["importedNames"])) {
 
                 // GET TITLE AND DATE 1st BECAUSE IF IT IS EMPTY OR <2014, JUST SKIP
                 if (isset($paper->date))         { $date = $paper->date; } else { $date = "NULL";}
-                if (isset($paper->title))        { $title = '"'.$paper->title.'"'; } else { $title = "NULL";}
+                if (isset($paper->title))        { $title = '"'.addslashes($paper->title).'"'; } else { $title = "NULL";}
                 if ($date != "NULL") {
                     if (strlen($date)==4) {              // only year
                         $date = $date . "-01-01";
@@ -55,64 +55,86 @@ if(!isset($_SESSION["importedNames"]) && empty($_SESSION["importedNames"])) {
 
                     $split_date = explode('-',$date);
                     $year = $split_date[0];
-                    if ($title!="NULL" && $year>=2014){
-                        $date = '"'.$date.'"';           // add quotes for DB INSERT
+                    if (!empty($paper->creators) && isset($paper->creators) && property_exists($paper, creators)) {
+                        if ($title!="NULL" && $year>=2014){
+                            $date = '"'.$date.'"';           // add quotes for DB INSERT
 
 
-                        if (isset($paper->type))         { $type = '"'.str_replace('"', "'", $paper->type).'"'; }
-                        if (isset($paper->creators))     { $allcreators = $paper->creators; }
-                        if (isset($paper->succeeds))     { $succeeds = $paper->succeeds; } else { $succeeds = "NULL";}
-                        if (isset($paper->ispublished))  { $ispublished = '"'.str_replace('"', "'", $paper->ispublished).'"'; } else { $ispublished = "NULL";}
-                        if (isset($paper->pres_type))    { $presType = '"'.str_replace('"', "'", $paper->pres_type).'"'; } else { $presType = "NULL";}
-                        if (isset($paper->keywords))     { $keywords = '"'.str_replace('"', "'", $paper->keywords).'"'; } else { $keywords = "NULL";}
-                        if (isset($paper->publication))  { $publication = '"'.str_replace('"', "'", $paper->publication).'"'; } else { $publication = "NULL";}
-                        if (isset($paper->volume))       { $volume = '"'.str_replace('"', "'", $paper->volume).'"'; } else { $volume = "NULL";}
-                        if (isset($paper->number))       { $number = '"'.str_replace('"', "'", $paper->number).'"'; } else { $number = "NULL";}
-                        if (isset($paper->publisher))    { $publisher = '"'.str_replace('"', "'", $paper->publisher).'"'; } else { $publisher = "NULL";}
-                        if (isset($paper->event_title))  { $eventTitle = '"'.str_replace('"', "'", $paper->event_title).'"'; } else { $eventTitle = "NULL";}
-                        if (isset($paper->event_type))   { $eventType = '"'.str_replace('"', "'", $paper->event_type).'"'; } else { $eventType = "NULL";}
-                        if (isset($paper->isbn))         { $isbn = '"'.str_replace('"', "'", $paper->isbn).'"'; } else { $isbn = "NULL";}
-                        if (isset($paper->issn))         { $issn = '"'.str_replace('"', "'", $paper->issn).'"'; } else { $issn = "NULL";}
-                        if (isset($paper->book_title))   { $bookTitle = '"'.str_replace('"', "'", $paper->book_title).'"'; } else { $bookTitle = "NULL";}
-                        if (isset($paper->eprintid))     { $eprintid = $paper->eprintid; } else { $eprintid = "NULL";}
-                        if (isset($paper->official_url)) { $doi = '"'.str_replace('"', "'", $paper->official_url).'"'; } else { $doi = "NULL";}
-                        if (isset($paper->uri))          { $uri = '"'.str_replace('"', "'", $paper->uri).'"'; } else { $uri = "NULL";}
-                        if (isset($paper->abstract))     { $abstract = '"'.str_replace('"', "'", $paper->abstract).'"'; } else { $abstract = "NULL";}
+    //                        if (isset($paper->type))         { $type = '"'.str_replace('"', "'", $paper->type).'"'; }
+    //                        if (isset($paper->creators))     { $allcreators = $paper->creators; }
+    //                        if (isset($paper->succeeds))     { $succeeds = $paper->succeeds; } else { $succeeds = "NULL";}
+    //                        if (isset($paper->ispublished))  { $ispublished = '"'.str_replace('"', "'", $paper->ispublished).'"'; } else { $ispublished = "NULL";}
+    //                        if (isset($paper->pres_type))    { $presType = '"'.str_replace('"', "'", $paper->pres_type).'"'; } else { $presType = "NULL";}
+    //                        if (isset($paper->keywords))     { $keywords = '"'.str_replace('"', "'", $paper->keywords).'"'; } else { $keywords = "NULL";}
+    //                        if (isset($paper->publication))  { $publication = '"'.str_replace('"', "'", $paper->publication).'"'; } else { $publication = "NULL";}
+    //                        if (isset($paper->volume))       { $volume = '"'.str_replace('"', "'", $paper->volume).'"'; } else { $volume = "NULL";}
+    //                        if (isset($paper->number))       { $number = '"'.str_replace('"', "'", $paper->number).'"'; } else { $number = "NULL";}
+    //                        if (isset($paper->publisher))    { $publisher = '"'.str_replace('"', "'", $paper->publisher).'"'; } else { $publisher = "NULL";}
+    //                        if (isset($paper->event_title))  { $eventTitle = '"'.str_replace('"', "'", $paper->event_title).'"'; } else { $eventTitle = "NULL";}
+    //                        if (isset($paper->event_type))   { $eventType = '"'.str_replace('"', "'", $paper->event_type).'"'; } else { $eventType = "NULL";}
+    //                        if (isset($paper->isbn))         { $isbn = '"'.str_replace('"', "'", $paper->isbn).'"'; } else { $isbn = "NULL";}
+    //                        if (isset($paper->issn))         { $issn = '"'.str_replace('"', "'", $paper->issn).'"'; } else { $issn = "NULL";}
+    //                        if (isset($paper->book_title))   { $bookTitle = '"'.str_replace('"', "'", $paper->book_title).'"'; } else { $bookTitle = "NULL";}
+    //                        if (isset($paper->eprintid))     { $eprintid = $paper->eprintid; } else { $eprintid = "NULL";}
+    //                        if (isset($paper->official_url)) { $doi = '"'.str_replace('"', "'", $paper->official_url).'"'; } else { $doi = "NULL";}
+    //                        if (isset($paper->uri))          { $uri = '"'.str_replace('"', "'", $paper->uri).'"'; } else { $uri = "NULL";}
+    //                        if (isset($paper->abstract))     { $abstract = '"'.str_replace('"', "'", $paper->abstract).'"'; } else { $abstract = "NULL";}
 
-                        if ($issn != "NULL") {
-                            $eraRating = checkEra2010rank($issn);       // check ERA2010 rank based on ISSN
-                        }
+                            if (isset($paper->type))         { $type = '"'.addslashes($paper->type).'"'; }
+                            if (isset($paper->creators))     { $allcreators = $paper->creators; }
+                            if (isset($paper->succeeds))     { $succeeds = $paper->succeeds; } else { $succeeds = "NULL";}
+                            if (isset($paper->ispublished))  { $ispublished = '"'.addslashes($paper->ispublished).'"'; } else { $ispublished = "NULL";}
+                            if (isset($paper->pres_type))    { $presType = '"'.addslashes($paper->pres_type).'"'; } else { $presType = "NULL";}
+                            if (isset($paper->keywords))     { $keywords = '"'.addslashes($paper->keywords).'"'; } else { $keywords = "NULL";}
+                            if (isset($paper->publication))  { $publication = '"'.addslashes($paper->publication).'"'; } else { $publication = "NULL";}
+                            if (isset($paper->volume))       { $volume = '"'.addslashes($paper->volume).'"'; } else { $volume = "NULL";}
+                            if (isset($paper->number))       { $number = '"'.addslashes($paper->number).'"'; } else { $number = "NULL";}
+                            if (isset($paper->publisher))    { $publisher = '"'.addslashes($paper->publisher).'"'; } else { $publisher = "NULL";}
+                            if (isset($paper->event_title))  { $eventTitle = '"'.addslashes($paper->event_title).'"'; } else { $eventTitle = "NULL";}
+                            if (isset($paper->event_type))   { $eventType = '"'.addslashes($paper->event_type).'"'; } else { $eventType = "NULL";}
+                            if (isset($paper->isbn))         { $isbn = '"'.addslashes($paper->isbn).'"'; } else { $isbn = "NULL";}
+                            if (isset($paper->issn))         { $issn = '"'.addslashes($paper->issn).'"'; } else { $issn = "NULL";}
+                            if (isset($paper->book_title))   { $bookTitle = '"'.addslashes($paper->book_title).'"'; } else { $bookTitle = "NULL";}
+                            if (isset($paper->eprintid))     { $eprintid = $paper->eprintid; } else { $eprintid = "NULL";}
+                            if (isset($paper->official_url)) { $doi = '"'.addslashes($paper->official_url).'"'; } else { $doi = "NULL";}
+                            if (isset($paper->uri))          { $uri = '"'.addslashes($paper->uri).'"'; } else { $uri = "NULL";}
+                            if (isset($paper->abstract))     { $abstract = '"'.addslashes($paper->abstract).'"'; } else { $abstract = "NULL";}
+
+                            if ($issn != "NULL") {
+                                $eraRating = checkEra2010rank($issn);       // check ERA2010 rank based on ISSN
+                            }
 
 
-                        // ONLY ADD TO DB IF IT HAS AN AUTHOR
-                        if(sizeof($allcreators)>0){
-                            foreach($allcreators as $eachcreator){
-                                $fName = $eachcreator->name->given;
-                                $lName = $eachcreator->name->family;
-                                $email = strtolower($eachcreator->id);
+                            // ONLY ADD TO DB IF IT HAS AN AUTHOR
+                            if(sizeof($allcreators)>0){
+                                foreach($allcreators as $eachcreator){
+                                    $fName = $eachcreator->name->given;
+                                    $lName = $eachcreator->name->family;
+                                    $email = strtolower($eachcreator->id);
 
-                                if ($formEmail == $email) {             // same author as the one in the uploaded spreadsheet
-                                    $mdxAuthorId = getMdxAuthorId($projectDetails, $fName, $lName, $email, $formEmail, $formCurrentEmployee);
-                                } else {
-                                    $mdxAuthorId = getMdxAuthorId($projectDetails, $fName, $lName, $email, $formEmail, null);
-                                }
-
-
-                                // CHECK IF PUBLICATION + AUTHOR ALREADY IN DB
-                                $publicationAlreadyInDB = checkPublicationAlreadyInDB ($projectDetails, $mdxAuthorId, $eprintid);
-    //                            echo "Publication + Author already in the DB? '$publicationAlreadyInDB'. Should show nothing if FALSE and 1 if true <br>";
-                                if (!$publicationAlreadyInDB){
-                                    $sql = "INSERT INTO `publication` (`projectID`,`type`,`author`,`succeeds`,`title`,`isPublished`,`presType`,`keywords`,`publication`,`volume`,`number`,`publisher`,`eventTitle`,`eventType`,`isbn`,`issn`,`bookTitle`,`ePrintID`,`doi`,`uri`, `abstract`,`date`,`eraRating`) VALUES ($projectDetails[0], $type, $mdxAuthorId, $succeeds, $title, $ispublished, $presType, $keywords, $publication, $volume, $number, $publisher, $eventTitle, $eventType, $isbn, $issn, $bookTitle, $eprintid, $doi, $uri, $abstract, $date, $eraRating);";
-                                    if ($conn->query($sql) === TRUE) {
-    //                                    echo "New record created successfully. Publication added. Author ID: " . $mdxAuthorId." - Publication ID: ".$eprintid."<br>";
+                                    if ($formEmail == $email) {             // same author as the one in the uploaded spreadsheet
+                                        $mdxAuthorId = getMdxAuthorId($projectDetails, $fName, $lName, $email, $formEmail, $formCurrentEmployee);
                                     } else {
-                                        echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
+                                        $mdxAuthorId = getMdxAuthorId($projectDetails, $fName, $lName, $email, $formEmail, null);
                                     }
-                                } else {
-    //                                echo "Publication + Author already in the DB. Nothing changed. Author ID: " . $mdxAuthorId." -  Publication ID: ".$eprintid."<br>";
+
+
+                                    // CHECK IF PUBLICATION + AUTHOR ALREADY IN DB
+                                    $publicationAlreadyInDB = checkPublicationAlreadyInDB ($projectDetails, $mdxAuthorId, $eprintid);
+        //                            echo "Publication + Author already in the DB? '$publicationAlreadyInDB'. Should show nothing if FALSE and 1 if true <br>";
+                                    if (!$publicationAlreadyInDB){
+                                        $sql = "INSERT INTO `publication` (`projectID`,`type`,`author`,`succeeds`,`title`,`isPublished`,`presType`,`keywords`,`publication`,`volume`,`number`,`publisher`,`eventTitle`,`eventType`,`isbn`,`issn`,`bookTitle`,`ePrintID`,`doi`,`uri`, `abstract`,`date`,`eraRating`) VALUES ($projectDetails[0], $type, $mdxAuthorId, $succeeds, $title, $ispublished, $presType, $keywords, $publication, $volume, $number, $publisher, $eventTitle, $eventType, $isbn, $issn, $bookTitle, $eprintid, $doi, $uri, $abstract, $date, $eraRating);";
+                                        if ($conn->query($sql) === TRUE) {
+        //                                    echo "New record created successfully. Publication added. Author ID: " . $mdxAuthorId." - Publication ID: ".$eprintid."<br>";
+                                        } else {
+                                            echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
+                                        }
+                                    } else {
+        //                                echo "Publication + Author already in the DB. Nothing changed. Author ID: " . $mdxAuthorId." -  Publication ID: ".$eprintid."<br>";
+                                    }
+                                    $totalPapersPerPerson++;
+        //                            echo "<hr>";
                                 }
-                                $totalPapersPerPerson++;
-    //                            echo "<hr>";
                             }
                         }
                     } else {
