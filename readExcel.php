b091a1aad109d3eed4df6f576e5d01a345003cfd
Cristiano Maia <criscmaia@gmail.com>
2018-06-18 15:43:31 +0100

    most of the debug code removed

--- readExcel.php
+++ readExcel.php
@@ -39,39 +39,29 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
             </tr>
         </thead>
         <tbody>
-            <?php
+<?php
     $authorsId = 1;
     foreach($allAuthors as $author) {
-//        echo $author->printAll() . "<br><br>";
-
         // get total of publications per author
         $link="http://eprints.mdx.ac.uk/cgi/search/archive/simple/export_mdx_JSON.js?output=JSON&exp=0|1|-|q3:creators_name/editors_name:ALL:EQ:".rawurlencode($author->getFullName());
-        echo "link: <pre>" . $link . "</pre><hr>";
-
         $result = mb_convert_encoding(file_get_contents($link), 'HTML-ENTITIES', "UTF-8");
-//        echo "result: <pre>" . $result . "</pre><hr>";
 
         // removing extra comma after the last creator array value
-        $search = "/" . "\"\s+\}\,\s+\}" . "/";                         // ending quote for family, break line, bracket and comma, break line, another bracket
-        $resultCleaned = preg_replace($search, "\"\n}\n}", $result);
-        $eprintsData_str = $resultCleaned;
-//        echo "jsonData: <pre>" . $eprintsData_str . "</pre><hr>";
-
-
+        $search = "/" . "\"\s+\}\,\s+\}" . "/";                             // regex for ending quote for family, break line, bracket and comma, break line, another bracket
+        $eprintsData_str = preg_replace($search, "\"\n}\n}", $result);
         $eprintsData = json_decode($eprintsData_str, true);
 
-        for ($i = 0; $i < count($eprintsData); $i++) {
-            foreach($eprintsData[$i] as $key => $value) {
-//                echo $key . "<br>";
-                if(strpos($key, 'rioxx2_') === 0 || strpos($key, 'hoa_') === 0  || strpos($key, 'documents') === 0 || strpos($key, 'dates') === 0 || strpos($key, 'files') === 0) {
-                    unset($eprintsData[$i][$key]);
+        // check if JSON is valid
+        if (json_last_error() === JSON_ERROR_NONE) {
+            // go through each paper removing unnecessary fields
+            for ($i = 0; $i < count($eprintsData); $i++) {
+                foreach($eprintsData[$i] as $key => $value) {
+                    if(strpos($key, 'rioxx2_') === 0 || strpos($key, 'hoa_') === 0  || strpos($key, 'documents') === 0 || strpos($key, 'dates') === 0 || strpos($key, 'files') === 0) {
+                        unset($eprintsData[$i][$key]);
+                    }
                 }
             }
-        }
 
-
-        if (json_last_error() === JSON_ERROR_NONE) {
-//            echo "JSON is valid <br>";
         } else {
             echo "JSON is NOT valid <br>";
         }
@@ -79,17 +69,6 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
         /*
         highlight_string("<?php\n\$data =\n" . var_export($eprintsData, true) . ";\n?>");
         */
-
-        echo "result: <pre>" . json_encode($eprintsData) . "</pre><hr>";
-
-
-        for ($i = 0; $i <= 1; $i++) {
-//            echo "creators: <pre>" . var_export($eprintsData[$i]['creators'], true) . "</pre><hr>";
-//            echo var_export($eprintsData[$i]['creators'], true);
-        }
-
-//        $eprintsDataData = json_encode($eprintsData, JSON_PRETTY_PRINT);
-//        echo "jsonData: <pre>" . $eprintsData . "</pre><hr>";
 
         echo '<tr>';
             echo '<td>' . $authorsId++ . '</td>';
