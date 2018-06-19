c8a7f3fa325fdd640d10845802d229df92fdbb77
Cristiano Maia <criscmaia@gmail.com>
2018-06-19 15:18:01 +0100

    too many changes :x

--- readExcel.php
+++ readExcel.php
@@ -48,9 +48,51 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
         $link="http://eprints.mdx.ac.uk/cgi/search/archive/simple/export_mdx_JSON.js?output=JSON&exp=0|1|-|q3:creators_name/editors_name:ALL:EQ:".rawurlencode($author->getFullName());
         $result = mb_convert_encoding(file_get_contents($link), 'HTML-ENTITIES', "UTF-8");          // get the data from the ePrints result
         $eprintsDataObj = json_decode($result, true);                                               // Takes a JSON encoded string and converts it into a PHP variable
+
+
         if (json_last_error() === JSON_ERROR_NONE) {                                                // if JSON is valid
             if (count($eprintsDataObj)>0) {                                                         // if at least one result is available
-                $eprintsDataJSON = json_encode($eprintsDataObj, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);  // Returns the JSON representation of a value
+//                for ($i = 0; $i < count($eprintsDataObj); $i++) {                                   // go through each paper
+                foreach($eprintsDataObj as $keyObj => $valueObj) {
+                    // GET TITLE AND DATE 1st BECAUSE IF IT IS EMPTY OR <2014, JUST SKIP
+                    if (isset($eprintsDataObj[$i]['date']))         { $date = $eprintsDataObj[$i]['date']; } else { $date = "NULL";}
+                    if (isset($eprintsDataObj[$i]['title']))        { $title = $eprintsDataObj[$i]['title']; } else { $title = "NULL";}
+
+                    echo "<p>Date: $date. Title: $title</p>";
+
+                    if ($date != "NULL") {
+                        if (strlen($date)==4) {              // only year
+                            $date = $date . "-01-01";
+                        } else if (strlen($date)==7) {       // only year and month
+                            $date = $date . "-01";
+                        }
+                        $split_date = explode('-',$date);
+                        $year = $split_date[0];
+                        if ($title=="NULL" || $year<2014){  // remove because title is null OR date is < 2014
+                            echo "<p>Removing paper because title is null OR date is < 2014</p>";
+//                            print_r($eprintsDataObj[0]);
+                            unset($eprintsDataObj[$i]);
+                            echo "<h1>".count($eprintsDataObj)."</h1>";
+                            continue;
+                        }
+                    } else {                                // remove because no date is set
+                        echo "<h4>Removing paper because no date is set</h4>";
+//                        unset($eprintsDataObj[$i]);
+                    }
+
+
+                    foreach($eprintsDataObj[$keyObj] as $key => $value) {                                // go through each author
+
+
+//                        if () {                                                                     // remove paper if date is null or not >= 2014
+//                            unset($eprintsDataObj[$i]);
+//                        } else
+                        if(strpos($key, 'rioxx2_') === 0 || strpos($key, 'hoa_') === 0  || strpos($key, 'documents') === 0 || strpos($key, 'dates') === 0 || strpos($key, 'files') === 0) {     // remove unnecessary fields from valid papers
+                            unset($eprintsDataObj[$i][$key]);
+                        }
+                    }
+                }
+                $eprintsDataJSON = json_encode($eprintsDataObj, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);      // Returns the JSON representation of a value
                 echo "<pre>" . $eprintsDataJSON . "</pre><hr>";
             } else {
                 echo "No results found for <strong>".$author->getFullName()."</strong><hr>";
@@ -60,6 +102,7 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
         }
 
 
+//        $eprintsDataJSON
 
 //        $eprintsDataJSON = json_encode($result, true);
 //
