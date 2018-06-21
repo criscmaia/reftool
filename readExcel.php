d17babc0b0156de8eef3c4f17b369efce91752d2
Cristiano Maia <criscmaia@gmail.com>
2018-06-21 15:19:07 +0100

    debug on again.

--- readExcel.php
+++ readExcel.php
@@ -57,13 +57,13 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
     // loop all authors from the excel
     foreach($allAuthors as $author) {
         if ($author->getFirstName() == "Maeve") {                                              // Only found case where reverse search doesn't work
-//            echo "Searching for <strong>".$author->getFullName()."</strong>... ";
+            echo "Searching for <strong>".$author->getFullName()."</strong>... ";
             $link="http://eprints.mdx.ac.uk/cgi/search/archive/simple/export_mdx_JSON.js?output=JSON&exp=0|1|-|q3:creators_name/editors_name:ALL:EQ:".rawurlencode($author->getFullName());
         } else {
-//            echo "Searching for <strong>".$author->getFullNameReverse()."</strong>... ";
+            echo "Searching for <strong>".$author->getFullNameReverse()."</strong>... ";
             $link="http://eprints.mdx.ac.uk/cgi/search/archive/simple/export_mdx_JSON.js?output=JSON&exp=0|1|-|q3:creators_name/editors_name:ALL:EQ:".rawurlencode($author->getFullNameReverse());
         }
-//        echo $link . "<br>";
+        echo $link . "<br>";
         $result = mb_convert_encoding(file_get_contents($link), 'HTML-ENTITIES', "UTF-8");     // get the data from the ePrints result
         $papersObj = json_decode($result, true);                                               // Takes a JSON encoded string and converts it into a PHP variable
 
@@ -97,8 +97,9 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
 
                     foreach($papersObj[$papersObjKeys]['creators'] as $creatorsKeys => $creatorsValues) {       // for each author of each paper
                         $creatorFullName = ($papersObj[$papersObjKeys]['creators'][$creatorsKeys]['name']['given']." ".$papersObj[$papersObjKeys]['creators'][$creatorsKeys]['name']['family']);        // get the creator full name
-//                        echo $author->getFullName() ." - ".$creatorFullName."? creatorsKeys: $creatorsKeys <br><hr>";
+                        echo $author->getFullName() ." - ".$creatorFullName."? creatorsKeys: $creatorsKeys <br><hr>";
                         if(startsWith($creatorFullName, $author->getFirstName()) && endsWith($creatorFullName, $author->getLastName())) {        // double check if author is one of the creators
+                            echo "true <br>";
                             if ($creatorsKeys==0) {                                                             // if first authors
                                 $author->totalOfPublicationsFirstAuthor++;
                             } else {                                                                            // if co-author
@@ -115,14 +116,14 @@ if ( $xlsx = SimpleXLSX::parse($filePath)) {
                 }
                 $eprintsDataJSON = json_encode($papersObj, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);      // Returns the JSON representation of a value
             } else {
-//                echo "No results found for <strong>".$author->getFullName()."</strong><br>";
+                echo "No results found for <strong>".$author->getFullName()."</strong><br>";
             }
         } else {
             echo "JSON for <strong>".$author->getFullName()."</strong> is NOT valid <hr>";
         }
 
-//        echo "<strong>".count($papersObj)."</strong> valid papers found.</p>";
-//        echo "<pre>" . $eprintsDataJSON . "</pre><hr>";
+        echo "<strong>".count($papersObj)."</strong> valid papers found.</p>";
+        echo "<pre>" . $eprintsDataJSON . "</pre><hr>";
 
 
         /*
