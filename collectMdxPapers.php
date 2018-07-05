889a0c19e2675b564f40e907fd29a7285f0f5fcc
Cristiano Maia <criscmaia@gmail.com>
2018-07-05 19:00:05 +0100

    Processing whole publication set working! \o/ \o/

--- collectMdxPapers.php
+++ collectMdxPapers.php
@@ -20,35 +20,11 @@ if(!isset($_SESSION["publications"]) && empty($_SESSION["publications"])) {
             foreach($searchedAuthor as $searchedAuthorKeys => $searchedAuthorPublications) {                     // see all the authors
                 foreach($searchedAuthor[$searchedAuthorKeys] as $publicationKey => $publicationDetails) {        // go through the author's publications
 
-//                        `projectID`,
-//                        `publicationID`,
-//                    `type`,
-//                        `author`,
-//                    `succeeds`,
-//                    `title`,
-//                    `isPublished`,
-//                    `presType`,
-//                    `keywords`,
-//                    `publication`,
-//                    `volume`,
-//                    `number`,
-//                    `publisher`,
-//                    `eventTitle`,
-//                    `eventType`,
-//                    `isbn`,
-//                    `issn`,
-//                    `bookTitle`,
-//                    `ePrintID`,
-//                    `doi`,
-//                    `uri`,
-//                        `additionalInfo`,
-//                    `abstract`,
-//                    `date`,
-//                    `eraRating`
-
+                    // get value or set to null
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['type']))         { $type        = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['type']).'"'; }
+                    if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['creators']))     { $allcreators = $searchedAuthor[$searchedAuthorKeys][$publicationKey]['creators']; } else { $allcreators = "NULL";}                          // minor scenarios where creator is null
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['succeeds']))     { $succeeds    = $searchedAuthor[$searchedAuthorKeys][$publicationKey]['succeeds']; } else { $succeeds = "NULL";}
-                    if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['title']))        { $title = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['title']).'"'; } else { $title = "NULL";}
+                    if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['title']))        { $title       = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['title']).'"'; } else { $title = "NULL";}
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['ispublished']))  { $ispublished = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['ispublished']).'"'; } else { $ispublished = "NULL";}
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['pres_type']))    { $presType    = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['pres_type']).'"'; } else { $presType = "NULL";}
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['keywords']))     { $keywords    = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['keywords']).'"'; } else { $keywords = "NULL";}
@@ -66,19 +42,37 @@ if(!isset($_SESSION["publications"]) && empty($_SESSION["publications"])) {
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['uri']))          { $uri         = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['uri']).'"'; } else { $uri = "NULL";}
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['abstract']))     { $abstract    = '"'.addslashes($searchedAuthor[$searchedAuthorKeys][$publicationKey]['abstract']).'"'; } else { $abstract = "NULL";}
                     if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['date']))         { $date = $searchedAuthor[$searchedAuthorKeys][$publicationKey]['date']; } else { $date = "NULL";}
+                    if ($date != "NULL") {
+                        if (strlen($date)==4) {                 // only year
+                            $date = $date . "-01-01";
+                        } else if (strlen($date)==7) {          // only year and month
+                            $date = $date . "-01";
+                        }
+
+                        $date = "'".$date."'";
+                    }
                     if ($issn != "NULL")  /* check ERA from issn */                                   { $eraRating = checkEra2010rank($issn); }
 
-                    if (isset($searchedAuthor[$searchedAuthorKeys][$publicationKey]['creators']))     { $allcreators = $searchedAuthor[$searchedAuthorKeys][$publicationKey]['creators']; } else { $allcreators = "NULL";}                          // minor scenarios where creator is null
-
-                    echo "$type - $succeeds - $title - $ispublished - $presType - $keywords - $publication - $volume - $number - $publisher - $eventTitle - $eventType - $isbn - $issn - $bookTitle - $doi - $uri - $abstract - $date - $eraRating - ".print_r($allcreators)."<br><hr>";
-
-                }
-            }
-        }
+                    foreach($allcreators as $mdxAuthorId) {                                                                     // for each author from the publication
+                        $publicationAlreadyInDB = checkPublicationAlreadyInDB ($projectDetails, $mdxAuthorId, $eprintid);       // CHECK IF PUBLICATION + AUTHOR ALREADY IN DB
+                        if (!$publicationAlreadyInDB && !empty($mdxAuthorId)){
+                            $sql = "INSERT INTO `publication` (`projectID`,`type`,`author`,`succeeds`,`title`,`isPublished`,`presType`,`keywords`,`publication`,`volume`,`number`,`publisher`,`eventTitle`,`eventType`,`isbn`,`issn`,`bookTitle`,`ePrintID`,`doi`,`uri`, `abstract`,`date`,`eraRating`) VALUES ($projectDetails[0], $type, $mdxAuthorId, $succeeds, $title, $ispublished, $presType, $keywords, $publication, $volume, $number, $publisher, $eventTitle, $eventType, $isbn, $issn, $bookTitle, $eprintid, $doi, $uri, $abstract, $date, $eraRating);";
+                            if ($conn->query($sql) === TRUE) {
+                                echo "New record created successfully. Publication added. Author ID: " . $mdxAuthorId." - Publication ID: ".$eprintid."<br>";
+                            } else {
+                                echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
+                            }
+                        } else {
+                            echo "Publication + Author already in the DB. Nothing changed. Author ID: " . $mdxAuthorId." -  Publication ID: ".$eprintid."<br>";
+                        }
+                    }
+                }       // end of looping author's publications
+            }           // end of looping authors
+        }               // end of if at least one paper
     } else {
         echo "Invalid JSON. <br>";
     }
-}
+}                       // end of if no publications save to SESSION
 
 // check paper rank
 function checkEra2010rank($issn) {
@@ -100,110 +94,19 @@ function checkEra2010rank($issn) {
     }
 }
 
+// check if publication + author already in the DB
+function checkPublicationAlreadyInDB ($projectDetails, $mdxAuthorId, $eprintid) {
+    include 'dbconnect.php';
 
+    if ($checkPublicationAlreadyInDB = $conn->query("SELECT * FROM reftool.publication WHERE  projectID = $projectDetails[0] AND author = $mdxAuthorId AND ePrintID = '$eprintid';")) {
+        $row_cnt = $checkPublicationAlreadyInDB->num_rows;
+        if($row_cnt>0) {
+            return true;
+        } else {
+            return false;
+        }
+        $checkPublicationAlreadyInDB->close();
+    }
+}
 
-//            foreach($papersObj as $paper){
-//                            // ONLY ADD TO DB IF IT HAS AN AUTHOR
-//                            if(sizeof($allcreators)>0 && $allcreators!="NULL"){
-//                                foreach($allcreators as $eachcreator){
-//                                    $fName = $eachcreator->name->given;
-//                                    $lName = $eachcreator->name->family;
-//                                    $email = strtolower($eachcreator->id);
-//
-//                                    if ($formEmail == $email) {             // same author as the one in the uploaded spreadsheet
-//                                        $mdxAuthorId = getMdxAuthorId($projectDetails, $fName, $lName, $email, $formEmail, $formCurrentEmployee);
-//                                    } else {
-//                                        $mdxAuthorId = getMdxAuthorId($projectDetails, $fName, $lName, $email, $formEmail, null);
-//                                    }
-//
-//
-//                                    // CHECK IF PUBLICATION + AUTHOR ALREADY IN DB
-//                                    $publicationAlreadyInDB = checkPublicationAlreadyInDB ($projectDetails, $mdxAuthorId, $eprintid);
-//        //                            echo "Publication + Author already in the DB? '$publicationAlreadyInDB'. Should show nothing if FALSE and 1 if true <br>";
-//                                    if (!$publicationAlreadyInDB && !empty($mdxAuthorId)){      // Olga	van den Akker was an example of someone with NULL creator and empty mdxAuthorId
-//                                        $sql = "INSERT INTO `publication` (`projectID`,`type`,`author`,`succeeds`,`title`,`isPublished`,`presType`,`keywords`,`publication`,`volume`,`number`,`publisher`,`eventTitle`,`eventType`,`isbn`,`issn`,`bookTitle`,`ePrintID`,`doi`,`uri`, `abstract`,`date`,`eraRating`) VALUES ($projectDetails[0], $type, $mdxAuthorId, $succeeds, $title, $ispublished, $presType, $keywords, $publication, $volume, $number, $publisher, $eventTitle, $eventType, $isbn, $issn, $bookTitle, $eprintid, $doi, $uri, $abstract, $date, $eraRating);";
-//                                        if ($conn->query($sql) === TRUE) {
-//        //                                    echo "New record created successfully. Publication added. Author ID: " . $mdxAuthorId." - Publication ID: ".$eprintid."<br>";
-//                                        } else {
-//                                            echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
-//                                        }
-//                                    } else {
-//        //                                echo "Publication + Author already in the DB. Nothing changed. Author ID: " . $mdxAuthorId." -  Publication ID: ".$eprintid."<br>";
-//                                    }
-//                                    $totalPapersPerPerson++;
-//        //                            echo "<hr>";
-//                                }
-//                            }
-//                        }
-////                    }
-//                    else {
-//        //                echo "Either Title is null or no publications after 2014. Date: $date . Title: $title <br>";
-//                    }
-//                } else {
-//        //            echo "Date is null. Date: $date . Title: $title<br>";
-//                }
-//            }
-//        }
-//        echo "Total of <strong>" . $totalPapersPerPerson . "</strong> combinations of publication+author (where this person is an author) found.<br><br>";
-//    }
-//    echo "<hr><strong>Search completed! </strong><br>";
-//    echo "<a href='/reftool/fullList.php'>Show full list →</a>";
-//}       // check if variable session is set
-//
-//
-//// check if publication + author already in the DB
-//function checkPublicationAlreadyInDB ($projectDetails, $mdxAuthorId, $eprintid) {
-//    include 'dbconnect.php';
-//
-//    if ($checkPublicationAlreadyInDB = $conn->query("SELECT * FROM reftool.publication WHERE  projectID = $projectDetails[0] AND author = $mdxAuthorId AND ePrintID = '$eprintid';")) {
-//        $row_cnt = $checkPublicationAlreadyInDB->num_rows;
-//        if($row_cnt>0) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//        $checkPublicationAlreadyInDB->close();
-//    }
-//}
-//
-//
-//
-//// check if author is on the DB
-//function getMdxAuthorId($projectDetails, $fname, $lname, $email, $formEmail, $formCurrentEmployee){
-//    include 'dbconnect.php';
-//
-//    $fullName = $fname . ' ' . $lname;
-//    $found = strpos($email, "@mdx.ac.uk");
-//
-//    if (isset($formCurrentEmployee)) {          // if spreadsheet says if current employee
-//        $currentEmployee = $formCurrentEmployee;
-//        $query = "SELECT * FROM mdxAuthor WHERE projectID = $projectDetails[0] AND CONCAT(firstName, ' ', lastName) LIKE '%$fullName%' OR email LIKE '%$email%';";
-//    } else if ($found) {                        // if email ends with @mdx.ac.uk
-//        $currentEmployee = 1;
-//        $query = "SELECT * FROM mdxAuthor WHERE projectID = $projectDetails[0] AND CONCAT(firstName, ' ', lastName) LIKE '%$fullName%' OR email LIKE '%$email%';";
-//    } else {                                    // considers that is not an employee
-//        $currentEmployee = 0;
-//        $query = "SELECT * FROM mdxAuthor WHERE projectID = $projectDetails[0] AND CONCAT(firstName, ' ', lastName) LIKE '%$fullName%';";      // does not search by email because many authors with '[ex-mdx]' email.
-//    }
-//
-//
-//    if ($checkMdxAuthorExistence = $conn->query($query)) {
-//        $row_cnt = $checkMdxAuthorExistence->num_rows;
-//
-//        if($row_cnt>0) {                                                // author found in the DB
-//            $resultsArray = $checkMdxAuthorExistence->fetch_assoc();
-//            return $resultsArray['mdxAuthorID'];
-//        } else {                                                        // author does NOT exist in the DB
-//            $sql = "INSERT INTO `mdxAuthor` (`projectID`,`firstName`,`lastName`,`email`,`repositoryName`,`currentEmployee`) VALUES('$projectDetails[0]', '$fname','$lname','$email','$fullName','$currentEmployee');";
-//            if ($conn->query($sql) === TRUE) {
-//                $last_id = $conn->insert_id;
-////                echo "New record created successfully. ID: ". $last_id. " - fullName: ".$fullName. "<br>";
-//                return $last_id;
-//            } else {
-//                echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
-//            }
-//        }
-//        $checkMdxAuthorExistence->close();
-//    }
-//}
 ?>
